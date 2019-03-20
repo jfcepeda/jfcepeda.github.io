3296d09f5cb1c1925ae6254e5b42ab2864444e03
Justin <justincepeda97@gmail.com>
2019-03-19 21:59:39 -0400

    NEW

---
+++ Subsites/Explore More/3-5-18/arizona/app.js
@@ -0,0 +1,27 @@
+var i = 0;
+var images = []; 
+var time = 4500;
+
+images[0] = 'img/img1.jpg';
+images[1] = 'img/img2.jpg';
+images[2] = 'img/img3.jpg';
+images[3] = 'img/img4.jpg';
+images[4] = 'img/img5.jpg';
+images[5] = 'img/img6.jpg';
+images[6] = 'img/img7.jpg';
+images[7] = 'img/img8.jpg';
+
+
+function changeImg() {
+  document.slide.src = images[i];
+
+  if(i < images.length - 1) {
+    i++;
+  } else {
+    i = 0;
+  }
+
+  setTimeout('changeImg()', time);
+}
+
+window.onload = changeImg;
